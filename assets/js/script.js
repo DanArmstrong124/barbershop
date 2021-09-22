0308670265e672d2554f5ce03f2863dfe086bf94
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-09-22 21:20:00 +0000

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 window.onscroll = function() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (scrollY > 50) {
     document.getElementById("logo").addClass('logo');
   } else {
     document.getElementById("logo").addClass('par-logo');
