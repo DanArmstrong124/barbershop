1b8bd6857154b5e6428aa9900a1b0ef12bee69f7
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-09-22 20:46:13 +0000

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,5 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-     //>=, not <=
-    if (scroll >= 500) {
+ if (scrollY >= 500) {
         //clearHeader, not clearheader - caps H
         $("#logo").addClass(".par-logo");
         $("#nav").addClass(".par-nav");
-    }
-}); //missing );
+    }; //missing );
