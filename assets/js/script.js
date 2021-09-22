269152f216bc89c79d94d3e19fe7493812ec050a
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-09-22 21:26:09 +0000

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 setInterval(function() {
     if (scrollY > 50) {
-        $("#logo").addClass('par-logo').removeClass('logo');
-    }
+        $("logo").addClass('par-logo');
+        $("logo").removeClass('logo');
+    };
 }, 50);
