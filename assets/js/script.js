823f2b532bd0017297c5bcdb32bc4ec6e6ff5534
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-09-23 12:00:44 +0000

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
+const logo = document.getElementById('logo');
+const navbar = document.getElementById('nav');
+
 window.onscroll = function() {
     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
     if(scrollPosition >= 200){
-        document.getElementById('logo').classList.add('par-logo');
-        document.getElementById('logo').classList.remove('logo');
+        logo.classList.add('par-logo');
+        logo.classList.remove('logo');
+        navbar.classList.add('par-nav');
+        navbar.classList.remove('nav');
     };
 
     if(scrollPosition <= 199){
-        document.getElementById('logo').classList.add("logo");
-        document.getElementById('logo').classList.remove("par-logo");
+        logo.classList.add("logo");
+        logo.classList.remove("par-logo");
+        navbar.classList.add('nav');
+        navbar.classList.remove('par-nav');
     };
 };
