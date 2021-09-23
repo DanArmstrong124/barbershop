7ecc5d8fe3468a6fbe0018a012cd55d59b83eaf8
DanArmstrong124 <danielarmstrong124@gmail.com>
2021-09-23 11:50:51 +0000

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 window.onscroll = function() {
     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    alert(scrollPosition);
+
+    if(scrollPosition >= 200){
+        document.getElementById('logo').addClass('par-logo');
+        document.getElementById('logo').removeClass('logo');
+    };
+
+    if(scrollPosition <= 199){
+        document.getElementById('logo').addClass('logo');
+        document.getElementById('logo').removeClass('par-logo');
+    };
 };
